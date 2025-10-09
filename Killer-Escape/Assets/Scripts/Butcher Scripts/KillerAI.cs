695b305812af360f8722920032c521f152cd3013
Pinqi Feng <jeff.pqf@gmail.com>
2025-10-09 00:14:05 -0400

    trying to make patrol

--- Killer-Escape/Assets/Scripts/Butcher Scripts/KillerAI.cs
+++ Killer-Escape/Assets/Scripts/Butcher Scripts/KillerAI.cs
@@ -3,20 +3,62 @@ using UnityEngine.AI;
 
 public class KillerAI : MonoBehaviour
 {
+
+
+    public Transform[] waypoints;
+    private int currentWaypointIndex = 0;
+
+    public Transform player;
+    public float detectionRange = 10f;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-    
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+
+        if (waypoints.Length > 0)
+        {
+            agent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Patrol();
+        PatrolLogic();
+        DetectPlayer();
     }
 
-    void Patrol()
+    void PatrolLogic()
     {
-        
+        animator.SetBool("isPatrolling", true);
+        animator.SetFloat("speed", agent.velocity.magnitude);
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            // Move to next waypoint
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            agent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+
+    void DetectPlayer()
+    {
+        float dist = Vector3.Distance(transform.position, player.position);
+        animator.SetFloat("distanceToPlayer", dist);
+
+        if (dist < detectionRange)
+        {
+            animator.SetBool("playerInSight", true);
+            animator.SetBool("isPatrolling", false);
+        }
+        else
+        {
+            animator.SetBool("playerInSight", false);
+        }
     }
 }
