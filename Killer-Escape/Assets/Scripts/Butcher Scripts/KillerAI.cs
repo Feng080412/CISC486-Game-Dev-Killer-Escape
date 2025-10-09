87983c44c76b548a99aacf1b70028cb16a7a0645
Pinqi Feng <jeff.pqf@gmail.com>
2025-10-08 23:59:12 -0400

    removed trap

--- Killer-Escape/Assets/Scripts/Butcher Scripts/KillerAI.cs
+++ Killer-Escape/Assets/Scripts/Butcher Scripts/KillerAI.cs
@@ -4,18 +4,9 @@ using UnityEngine.AI;
 public class KillerAI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Transform[] patrolPoints;
-    private int currentPatrolIndex = 0;
-
-    private NavMeshAgent agent;
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-
-        if (patrolPoints.Length > 0)
-        {
-            agent.SetDestination(patrolPoints[0].position);
-        }
+    
     }
 
     // Update is called once per frame
@@ -26,13 +17,6 @@ public class KillerAI : MonoBehaviour
 
     void Patrol()
     {
-        if (patrolPoints.Length == 0) return;
-
-        // If close to current target, go to next
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-        {
-            currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
-            agent.SetDestination(patrolPoints[currentPatrolIndex].position);
-        }
+        
     }
 }
